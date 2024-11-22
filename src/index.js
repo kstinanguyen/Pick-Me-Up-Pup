03e22d549f815032a02ea3182c8c5f222b40863c
Kristina N <87397396+kstinanguyen@users.noreply.github.com>
2024-11-22 11:20:39 -0800

    index.js populated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('loaded!');
+const NUMBER_OF_DOGS = 15; //global constants can use snake case, local variables will use camel case
+
+const state = {
+  dogImageContainer: null,
+  addDogButton: null,
+  dogCountLabel: null,
+  dogCount: 1,
+};
+
+const pickRandomNum = () => {
+  return Math.floor(Math.random() * NUMBER_OF_DOGS) + 1;
+};
+
+const loadControls = () => {
+  state.addDogButton = document.getElementById('add-dog-btn');
+  state.dogImageContainer = document.getElementById('dog-img-container');
+  state.dogCountLabel = document.getElementById('dog-count-label');
+};
+
+const randomDogImage = () => {
+  const imgNumber = pickRandomNum();
+  const newImgName = `../imgs/${imgNumber}.jpeg`;
+  const newDog = document.createElement('img');
+  newDog.src = newImgName;
+  newDog.alt = 'A random doggo';
+  ++state.dogCount;
+  return newDog;
+};
+
+const addDogImgToTop = () => {
+  const newDog = randomDogImage();
+  state.dogImageContainer.prepend(newDog);
+  state.dogCountLabel.textContent = state.dogCount;
+};
+
+const handleAddButtonClicked = (event) => {
+  addDogImgToTop();
+};
+
+const registerEventHandlers = () => {
+  loadControls();
+  // setRandomDog();
+  state.addDogButton.addEventListener('click', handleAddButtonClicked);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
